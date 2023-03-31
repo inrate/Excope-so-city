b329861d3c9a4dd1b22631de0500ce8029d1464c
AssAnKrak <madsrasmussen083@gmail.com>
2023-03-31 12:53:54 +0200

    made spike a2d, and made character again

---
+++ src/MEOW.gd
@@ -0,0 +1,28 @@
+extends KinematicBody2D
+
+const MAX_SPEED=200
+var FM = Sprite
+
+
+	
+	
+var velocity = Vector2.ZERO
+
+
+func _physics_process(delta):
+	var input_vector=Vector2.ZERO
+	input_vector.x=Input.get_action_strength("ui_right")-Input.get_action_strength("ui_left")
+	input_vector.y=Input.get_action_strength("ui_down")-Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	
+	if input_vector!=Vector2.ZERO:
+		velocity=velocity.move_toward(input_vector*MAX_SPEED,MAX_SPEED)
+	else:
+		velocity=velocity.move_toward(Vector2.ZERO,MAX_SPEED)
+	
+	velocity=(move_and_slide(velocity)*delta)
+
+
+func _on_Area2D_body_entered(body):
+	pass # Replace with function body.
